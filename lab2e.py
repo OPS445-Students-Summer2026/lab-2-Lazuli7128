da5139616498cd9098b5a216aaf90fb5a1769929
Lazuli Barba <lbarba-reancont@myseneca.ca>
2026-05-16 19:29:37 -0400

    Added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#Author: Lazuli Barba Reancont
+#Author ID: 105983225
+#Date Created: 2026/05/16
+
+timer = 10 
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
