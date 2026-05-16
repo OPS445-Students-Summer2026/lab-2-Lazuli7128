9c503830e3ac606417cf595ff7aec7b3f17f624d
Lazuli Barba <lbarba-reancont@myseneca.ca>
2026-05-16 19:30:17 -0400

    Added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Author: Lazuli Barba Reancont
+#Author ID: 105983225
+#Date Created: 2026/05/16
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
+
